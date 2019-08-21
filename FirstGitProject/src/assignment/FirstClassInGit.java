8c350acb6b691f8ad12207907ac743fdbdafb261
Js21914 <Js21914@CEIT2553214X011.ad.georgiasouthern.edu>
2019-08-21 16:43:51 -0400

    Added greeting sentence

--- FirstGitProject/src/assignment/FirstClassInGit.java
+++ FirstGitProject/src/assignment/FirstClassInGit.java
@@ -3,12 +3,13 @@ package assignment;
 public class FirstClassInGit {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		System.out.println("Hello World.");
 		
 		//add a statement to display your name in the console
 		
-		//Then commit your changes
-
+		System.out.println("My name is Jade");
+		//Then commit your changes  
+		
 	}
 
 }
